f19b23fc52a41ac36aa10a8d7d5ab2cc2af27c7f
Andre Jonas <nipsky@googlemail.com>
2023-01-07 23:15:26 +0100

    Load speaker names from external file

--- tts_convert.py
+++ tts_convert.py
@@ -72,121 +72,12 @@ class TTS_Item:
 class TTS_Convert:
     # Speakers in preferred order (usually #0 and #1 for normal and quote speaker)
     # Best speakers: p293, p330, p273
-    default_speakers = [
-        'p273',
-        'p330',
-        'p234',
-        'p270',
-        'p280',
-        'p308',
-        'p312',
-        'p318',
-        'p225',
-        'p226',
-        'p227',
-        'p228',
-        'p229',
-        'p230',
-        'p231',
-        'p232',
-        'p233',
-        'p236',
-        'p237',
-        'p238',
-        'p239',
-        'p240',
-        'p241',
-        'p243',
-        'p244',
-        'p245',
-        'p246',
-        'p247',
-        'p248',
-        'p249',
-        'p250',
-        'p251',
-        'p252',
-        'p253',
-        'p254',
-        'p255',
-        'p256',
-        'p257',
-        'p258',
-        'p259',
-        'p260',
-        'p261',
-        'p262',
-        'p263',
-        'p264',
-        'p265',
-        'p266',
-        'p267',
-        'p268',
-        'p269',
-        'p271',
-        'p272',
-        'p293',
-        'p274',
-        'p275',
-        'p276',
-        'p277',
-        'p278',
-        'p279',
-        'p281',
-        'p282',
-        'p283',
-        'p284',
-        'p285',
-        'p286',
-        'p287',
-        'p288',
-        'p292',
-        'p294',
-        'p295',
-        'p297',
-        'p298',
-        'p299',
-        'p300',
-        'p301',
-        'p302',
-        'p303',
-        'p304',
-        'p305',
-        'p306',
-        'p307',
-        'p310',
-        'p311',
-        'p313',
-        'p314',
-        'p316',
-        'p317',
-        'p323',
-        'p326',
-        'p329',
-        'p333',
-        'p334',
-        'p335',
-        'p336',
-        'p339',
-        'p340',
-        'p341',
-        'p343',
-        'p345',
-        'p347',
-        'p351',
-        'p360',
-        'p361',
-        'p362',
-        'p363',
-        'p364',
-        'p374',
-        'p376'
-    ]
+    default_speakers = []
 
     # Break lines after number of characters to avoid OOM
     max_chars = 320
 
-    def __init__(self, speakers=[str], model='tts_models/en/vctk/vits', vocoder='', multi=True) -> None:
+    def __init__(self, speakers=None, model='tts_models/en/vctk/vits', vocoder='', multi=True) -> None:
         self.model = model
         self.vocoder = vocoder
         self.multi = multi
@@ -195,6 +86,10 @@ class TTS_Convert:
 
         # self.quotes = quotes
         self.current_speaker_idx = 0
+
+        if not self.default_speakers:
+            with open(os.path.dirname(os.path.realpath(__file__)) + '/speakers', 'r') as speaker_file:
+                self.default_speakers = speaker_file.read().split()
 
         if speakers:
             self.default_speakers = speakers
