b9667f33cd49a9358d061a750d7dd398f7b14fec
Andre Jonas <nipsky@googlemail.com>
2023-04-27 21:57:24 +0200

    Import update

--- src/tts_arranger/__init__.py
+++ src/tts_arranger/__init__.py
@@ -1,3 +1,5 @@
 from .tts_processor import *
 from .tts_writer import *
 from .tts_simple_writer import *
+from .tts_reader import *
+from .tts_html_converter import *
---
+++ src/tts_arranger/tts_reader/__init__.py
@@ -0,0 +1,6 @@
+from .tts_abstract_reader import *
+from .tts_epub_reader import *
+from .tts_html_based_reader import *
+from .tts_html_reader import *
+from .tts_srt_reader import *
+from .tts_text_reader import *
