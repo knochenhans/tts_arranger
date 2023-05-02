2427824c3393391ada0bc86cfa0d03454959b4ea
Andre Jonas <nipsky@googlemail.com>
2023-05-02 19:00:42 +0200

    Made examples more platform-independent

--- src/tts_arranger/examples/tts_project_example.py
+++ src/tts_arranger/examples/tts_project_example.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+import os
 from tts_arranger import TTS_Item, TTS_Chapter, TTS_Project, TTS_Writer
+
+user_dir = os.path.expanduser('~')
 
 preferred_speakers = ['p273', 'p330']
 
@@ -24,7 +27,7 @@ project = TTS_Project(chapter, 'Project title', 'This is a subtitle', author='So
 project.add_image_from_url('https://coqui.ai/static/38a06ec53309f617be3eb3b8b9367abf/598c3/logo-wordmark.png')  # Add a cover image
 
 # Finally synthesize and write the project as a m4b audiobook using our preferred speakers
-writer = TTS_Writer(project, '/tmp/tts_arranger_example_output/', preferred_speakers=preferred_speakers)
+writer = TTS_Writer(project, os.path.join(user_dir, 'tts_arranger_example_output/'), preferred_speakers=preferred_speakers)
 writer.synthesize_and_write(project.author + ' - ' + project.title)
 
 # German example using Thorsten voice (no multispeaker support), output as mp3, writing one mp3 file per chapter
@@ -43,5 +46,6 @@ chapter.append(TTS_Chapter(items2, 'Kapitel 2'))
 
 project = TTS_Project(chapter, 'Projektname', 'Dies ist ein Untertitel', author='Ein Autor', lang_code='de')
 
-writer = TTS_Writer(project, '/tmp/tts_arranger_example_output/', model='tts_models/de/thorsten/tacotron2-DDC', vocoder='vocoder_models/de/thorsten/hifigan_v1', output_format='mp3')
+writer = TTS_Writer(project, os.path.join(user_dir, 'tts_arranger_example_output/'),
+                    model='tts_models/de/thorsten/tacotron2-DDC', vocoder='vocoder_models/de/thorsten/hifigan_v1', output_format='mp3')
 writer.synthesize_and_write(project.author + ' - ' + project.title, concat=False)
--- src/tts_arranger/examples/tts_simple_example.py
+++ src/tts_arranger/examples/tts_simple_example.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+import os
+
 from tts_arranger import TTS_Item, TTS_Simple_Writer
 
 # Simple example using Simple Writer (using a simple list of TTS items), uses tts_models/en/vctk/vits by (default)
 
 tts_items = []
+
+user_dir = os.path.expanduser('~')
 
 preferred_speakers = ['p273', 'p330']
 
@@ -13,4 +17,4 @@ tts_items.append(TTS_Item('This is a test with another speaker and a fixed minim
 
 # Create writer using our item list and prefered speakers and synthesize and save as mp3 audio
 simple_writer = TTS_Simple_Writer(tts_items, preferred_speakers)
-simple_writer.synthesize_and_write('/tmp/tts_arranger_example_output/test.mp3')
+simple_writer.synthesize_and_write(os.path.join(user_dir, 'tts_arranger_example_output/test.mp3'))
