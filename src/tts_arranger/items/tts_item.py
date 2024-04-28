4a0f55b2c4d3797fc8b1c782c982825803dd07aa
Andre <nipsky@googlemail.com>
2024-04-28 08:50:58 +0200

    json class method for items

--- src/tts_arranger/items/tts_item.py
+++ src/tts_arranger/items/tts_item.py
@@ -19,6 +19,23 @@ class TTS_Item():
     speaker_idx: int = 0
     length: int = 0
 
+    @classmethod
+    def from_json(cls, json_data: dict) -> 'TTS_Item':
+        """
+        Class method to load a TTS item from a JSON object.
+
+        :param json_data: A dictionary representing the JSON object to be loaded.
+        :type json_data: dict
+
+        :return: A TTS item object loaded from the JSON object.
+        :rtype: TTS_Item
+        """
+        return cls(
+            text=json_data.get('text', ''),
+            # speaker_idx=json_data.get('speaker_idx', 0),
+            length=json_data.get('min_length', 0),
+        )
+
     def __post_init__(self):
         # Mark pauses by invalidating speaker index
         if self.text == '' and self.length > 0:
