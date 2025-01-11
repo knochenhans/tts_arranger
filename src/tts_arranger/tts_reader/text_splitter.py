973cee4131134d84f20ad4de0c6e6007d68775a3
Andre Jonas <nipsky@googlemail.com>
2025-01-11 12:38:31 +0100

    Add TextSplitter class for splitting text using Gemini

---
+++ src/tts_arranger/tts_reader/text_splitter.py
@@ -0,0 +1,37 @@
+import ast
+import json
+from typing import Dict, List
+import google.generativeai as genai
+
+import typing_extensions as typing
+
+
+# Set up type for the response, should contain text and type of the text
+class TextItem(typing.TypedDict):
+    text: str
+    type: str
+
+
+class TextSplitterResponse(typing.TypedDict):
+    original_text: str
+    splitted_text: List[TextItem]
+
+
+class TextSplitter:
+    def __init__(self, api_key: str):
+        genai.configure(api_key=api_key)
+        self.model = genai.GenerativeModel("gemini-1.5-pro-latest")
+
+    def split_text(self, text: str) -> Dict:
+        prompt = "Split the following text when encountering a year number."
+        contents = prompt + "\n\n" + text
+
+        result = self.model.generate_content(
+            contents=contents,
+            generation_config=genai.GenerationConfig(
+                response_mime_type="application/json",
+                response_schema=TextSplitterResponse,
+            ),
+        )
+        
+        return ast.literal_eval(result.text).get("splitted_text")
