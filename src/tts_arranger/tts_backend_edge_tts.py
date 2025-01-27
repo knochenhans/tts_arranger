95aa25b8c44dbb95d9a899978d5d636a344fd2c3
Andre Jonas <nipsky@googlemail.com>
2025-01-27 11:16:56 +0100

    Added edge tts backend

---
+++ src/tts_arranger/tts_backend_edge_tts.py
@@ -0,0 +1,137 @@
+import io
+import os
+import random
+from typing import Any, Dict, List, Callable, Optional
+import numpy as np
+from platformdirs import user_data_dir
+import soundfile as sf  # type: ignore
+import time
+
+from tts_arranger.functions import load_default_voices  # type: ignore
+from .tts_backend import TTSBackend
+import edge_tts  # type: ignore
+from tqdm import tqdm
+from loguru import logger
+import unicodedata
+
+TextItem = Dict[str, str | float]
+
+
+class TTSBackendEdge(TTSBackend):
+    def __init__(
+        self,
+        env_name: str,
+        temp_dir: str,
+        backend_config: Dict[str, Any],
+        progress_callback: Optional[Callable[[int], None]] = None,
+    ):
+        self.env_name: str = env_name
+        self.temp_dir: str = os.path.join(temp_dir, "edge_tts")
+        self.backend_config: Dict[str, Any] = backend_config
+        self.progress_callback: Optional[Callable[[int], None]] = progress_callback
+
+        os.makedirs(self.temp_dir, exist_ok=True)
+
+        # if self.backend_config.get("")
+        self.voices: Dict[str, Dict[str, Any]] = load_default_voices(backend="edge-tts")
+
+        logger.info("Edge TTS backend initialized")
+
+    def cleanup(self) -> None:
+        pass
+
+    # def preprocess(
+    #     self, text_items: List[Dict[str, str | float]]
+    # ) -> List[Dict[str, str | float]]:
+        
+
+    def synthesize_batch(self, text_items: List[TextItem]) -> List[np.ndarray]:
+        numpy_waves: List[np.ndarray] = []
+
+        loop_obj = tqdm(text_items, desc="Synthesizing")
+
+        for i, text_item in enumerate(loop_obj):
+            if (
+                not text_item.get("text", "")
+                and float(text_item.get("min_length", 0)) > 0
+            ):
+                numpy_waves.append(
+                    np.zeros(
+                        int(
+                            text_item["min_length"]
+                            * self.backend_config.get("sample_rate", 24000)
+                            / 1000
+                        )
+                    )
+                )
+                continue
+
+            text: str = str(text_item.get("text", ""))
+
+            if text.strip() == "":
+                continue
+
+            # Continue if text only contains punctuation
+            if all(unicodedata.category(char).startswith("P") for char in text.strip()):
+                continue
+
+            # Continue if text does not contain any letters or digits
+            if not any(unicodedata.category(char).startswith(("L", "N")) for char in text.strip()):
+                continue
+
+            speaker_id: str = str(text_item.get("speaker_id", ""))
+
+            if speaker_id is None:
+                logger.error(f"Speaker ID {speaker_id} not found for text item {text_item}, using empty id")
+
+            speaker_id_mapping: Dict[str, str] = self.backend_config.get(
+                "speaker_id_mapping", {}
+            )
+
+            voice_ids = speaker_id_mapping.get(speaker_id, "")
+
+            if not voice_ids:
+                voice_id = list(self.voices.keys())[0]
+                logger.warning(f"No voice IDs found for speaker {speaker_id}, using {voice_id} instead")
+            else:
+                voice_id = random.choice(voice_ids)
+                # voice: Dict[str, Any] = self.voices.get(voice_id, {})
+
+            # if not voice_id:
+            #     raise ValueError(f"Voice {voice_id} not found for speaker {speaker_id}")
+
+            # logger.debug(f"Synthesizing text: {text} with voice: {voice_id}")
+
+            audio_bytes = io.BytesIO()
+
+            for attempt in range(3):  # Try up to 3 times
+                try:
+                    communicate = edge_tts.Communicate(text, voice_id)
+                    start_time = time.time()
+                    for chunk in communicate.stream_sync():
+                        if time.time() - start_time > 600:  # 10 minutes
+                            logger.error(f"Stream sync taking too long for text item: {text_item}")
+                            raise TimeoutError("Stream sync exceeded 10 minutes")
+                        if chunk["type"] == "audio":
+                            if "data" in chunk:
+                                audio_bytes.write(chunk["data"])
+                    break  # Exit loop if successful
+                except Exception as e:
+                    logger.error(f"Error synthesizing text: {e} for text: {text}")
+                    if attempt < 2:  # Wait only if it's not the last attempt
+                        logger.info("Retrying in 60 seconds...")
+                        time.sleep(60)
+                    else:
+                        logger.error("All attempts to synthesize text failed. Exiting.")
+                        raise SystemExit(
+                            e
+                        )  # Raise the exception and exit if all attempts fail
+
+            audio_bytes.seek(0)
+            audio_data, _ = sf.read(io.BytesIO(audio_bytes.getvalue()))
+            numpy_waves.append(audio_data)
+
+            if self.progress_callback:
+                self.progress_callback(i)
+
+        return numpy_waves
